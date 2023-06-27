afa52f6ce55babdf7cfa973bc5174245eeddfd50
evilaks <evilaks@gmail.com>
2023-06-27 19:01:42 +0400

    fix naming according to github tests

--- stat/stats-server/src/main/java/ru/practicum/repo/StatServerRepo.java
+++ stat/stats-server/src/main/java/ru/practicum/repo/StatServerRepo.java
@@ -14,27 +14,27 @@ public interface StatServerRepo extends JpaRepository<StatEvent, Long> {
 
     @Query("SELECT new ru.practicum.dto.StatisticsReportDto(e.app, e.uri, COUNT(e)) " +
             "FROM StatEvent e WHERE e.timestamp BETWEEN :startDate AND :endDate " +
-            "AND e.uri IN :uris GROUP BY e.app, e.uri")
+            "AND e.uri IN :uris GROUP BY e.app, e.uri ORDER BY COUNT(e) DESC")
     List<StatisticsReportDto> countEventsByDatesAndUris(@Param("startDate") LocalDateTime startDate,
                                                         @Param("endDate") LocalDateTime endDate,
                                                         @Param("uris") List<String> uris);
 
     @Query("SELECT new ru.practicum.dto.StatisticsReportDto(e.app, e.uri, COUNT(e)) " +
             "FROM StatEvent e WHERE e.timestamp BETWEEN :startDate AND :endDate " +
-            "GROUP BY e.app, e.uri")
+            "GROUP BY e.app, e.uri ORDER BY COUNT(e) DESC")
     List<StatisticsReportDto> countEventsByDates(@Param("startDate") LocalDateTime startDate,
                                                  @Param("endDate") LocalDateTime endDate);
 
     @Query("SELECT new ru.practicum.dto.StatisticsReportDto(e.app, e.uri, COUNT(DISTINCT e.ip))" +
             "FROM StatEvent e WHERE e.timestamp BETWEEN :startDate AND :endDate " +
-            "AND e.uri IN :uris GROUP BY e.app, e.uri")
+            "AND e.uri IN :uris GROUP BY e.app, e.uri ORDER BY COUNT(DISTINCT e.ip) DESC")
     List<StatisticsReportDto> countEventsByDatesAndUrisWithUniqueIps(@Param("startDate") LocalDateTime startDate,
                                                                      @Param("endDate") LocalDateTime endDate,
                                                                      @Param("uris") List<String> uris);
 
     @Query("SELECT new ru.practicum.dto.StatisticsReportDto(e.app, e.uri, COUNT(DISTINCT e.ip)) " +
             "FROM StatEvent e WHERE e.timestamp BETWEEN :startDate AND :endDate " +
-            "GROUP BY e.app, e.uri")
+            "GROUP BY e.app, e.uri ORDER BY COUNT(DISTINCT e.ip) DESC")
     List<StatisticsReportDto> countEventsByDatesWithUniqueIps(@Param("startDate") LocalDateTime startDate,
                                                               @Param("endDate") LocalDateTime endDate);
 
