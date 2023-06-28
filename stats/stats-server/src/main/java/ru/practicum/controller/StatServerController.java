85cad63db98100ecda82ccfb2418a69320b0e7ce
evilaks <evilaks@gmail.com>
2023-06-28 21:48:53 +0400

    refactor minor improvements after review

--- stats/stats-server/src/main/java/ru/practicum/controller/StatServerController.java
+++ stats/stats-server/src/main/java/ru/practicum/controller/StatServerController.java
@@ -2,6 +2,8 @@ package ru.practicum.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.dto.StatisticsEventDto;
 import ru.practicum.dto.StatisticsReportDto;
@@ -17,18 +19,18 @@ public class StatServerController {
     private final StatServerServiceImpl service;
 
     @PostMapping("/hit")
-    public StatisticsEventDto saveEvent(@RequestBody StatisticsEventDto statEventDto) {
+    public ResponseEntity<StatisticsEventDto> saveEvent(@RequestBody StatisticsEventDto statEventDto) {
         log.info("Saving event: {}", statEventDto);
-        return service.save(statEventDto);
+        return new ResponseEntity<>(service.save(statEventDto), HttpStatus.CREATED);
     }
 
 
     @GetMapping("/stats")
-    public List<StatisticsReportDto> getStatistics(@RequestParam (defaultValue = "1900-01-01 00:00:01") String start,
-                                                   @RequestParam (defaultValue = "2099-12-31 23:59:59") String end,
-                                                   @RequestParam (defaultValue = "ALL") List<String> uris,
-                                                   @RequestParam (defaultValue = "false") Boolean unique) {
+    public ResponseEntity<List<StatisticsReportDto>> getStatistics(@RequestParam (defaultValue = "1900-01-01 00:00:01") String start,
+                                                                  @RequestParam (defaultValue = "2099-12-31 23:59:59") String end,
+                                                                  @RequestParam (defaultValue = "ALL") List<String> uris,
+                                                                  @RequestParam (defaultValue = "false") Boolean unique) {
         log.info("Getting statistics for period {} - {}, uris: {}, unique: {}", start, end, uris, unique);
-        return service.getStatistics(start, end, uris, unique);
+        return ResponseEntity.ok().body(service.getStatistics(start, end, uris, unique));
     }
 }
