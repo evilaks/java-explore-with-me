3e8f9c785bd136f1d533823f68537c5af163e2d4
evilaks <evilaks@gmail.com>
2023-07-01 14:01:10 +0400

    fix small issues according to tests

--- ewm-main/ewm-service/src/main/java/ru/practicum/controller/auth/EventControllerAuth.java
+++ ewm-main/ewm-service/src/main/java/ru/practicum/controller/auth/EventControllerAuth.java
@@ -2,6 +2,7 @@ package ru.practicum.controller.auth;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.dto.event.EventFullDto;
@@ -23,7 +24,7 @@ public class EventControllerAuth {
     public ResponseEntity<EventFullDto> createEvent(@RequestBody NewEventDto event,
                                                     @PathVariable Long userId) {
         log.info("Creating event {} from user {}", event, userId);
-        return ResponseEntity.ok().body(eventService.createEvent(event, userId));
+        return new ResponseEntity<>(eventService.createEvent(event, userId), HttpStatus.CREATED);
     }
 
     // GET /users/{userId}/events/{eventId}
